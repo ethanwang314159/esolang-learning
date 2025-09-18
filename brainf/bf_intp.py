09a4aa9f7a4732d16465616c3ad5a2cd6f20c5a9
ethanwang314159@gmail.com <ethanwang314159@gmail.com>
2025-09-18 14:36:13 +1000

    interpreter fix

--- brainf/bf_intp.py
+++ brainf/bf_intp.py
@@ -1,59 +1,69 @@
-num_zeros = 10  
-tape = [0] * num_zeros
-tickerpos = 0
+import sys
+def bf(input):
 
-def remove_non_commands(code):
-    commands = set("><+-.,[]")
-    return ''.join(c for c in code if c in commands)
+    num_zeros = 100  
+    tape = [0] * num_zeros
+    tickerpos = 0
 
-code = remove_non_commands(input("> "))
-i = 0
-while i < len(code):
-    char = code[i]
-    
-    if char == '>':
-        tickerpos += 1
-        if tickerpos >= num_zeros:
-            tickerpos = 0
-    elif char == '<':
-        tickerpos -= 1
-        if tickerpos < 0:
-            tickerpos = num_zeros - 1
-    elif char == '+':
-        tape[tickerpos] = (tape[tickerpos] + 1) % 256
-    elif char == '-':
-        tape[tickerpos] = (tape[tickerpos] - 1) % 256
-    elif char == '.':
-        print(chr(tape[tickerpos]), end='')
-    elif char == ',':
-        inp = input("inp > ")
-        if inp:
-            tape[tickerpos] = ord(inp[0]) % 256
+    def remove_non_commands(code):
+        commands = set("><+-.,[]")
+        return ''.join(c for c in code if c in commands)
+
+    code = remove_non_commands(input("> "))
+    i = 0
+    while i < len(code):
+        char = code[i]
+        
+        if char == '>':
+            tickerpos += 1
+            if tickerpos >= num_zeros:
+                tickerpos = 0
+        elif char == '<':
+            tickerpos -= 1
+            if tickerpos < 0:
+                tickerpos = num_zeros - 1
+        elif char == '+':
+            tape[tickerpos] = (tape[tickerpos] + 1) % 256
+        elif char == '-':
+            tape[tickerpos] = (tape[tickerpos] - 1) % 256
+        elif char == '.':
+            print(chr(tape[tickerpos]), end='')
+        elif char == ',':
+            inp = input()
+            if inp:
+                tape[tickerpos] = ord(inp[0]) % 256
+            else:
+                tape[tickerpos] = 0
+        elif char == '[':
+            if tape[tickerpos] == 0:
+                    # find matching ]
+                    count = 1
+                    j = i
+                    while count > 0:
+                        
+                        j += 1
+                        if code[j] == "[":
+                            count += 1
+                        elif code[j] == "]":
+                            count -= 1
+        elif char == ']':
+            if tape[tickerpos] != 0:
+                    # find matching [
+                    count = 1
+                    i
+                    while count > 0:
+                        i -= 1
+                        if code[i] == "]":
+                            count += 1
+                        elif code[i] == "[":
+                            count -= 1
         else:
-            tape[tickerpos] = 0
-    elif char == '[':
-        if tape[tickerpos] == 0:
-                # find matching ]
-                count = 1
-                j = i
-                while count > 0:
-                    
-                    j += 1
-                    if code[j] == "[":
-                        count += 1
-                    elif code[j] == "]":
-                        count -= 1
-    elif char == ']':
-        if tape[tickerpos] != 0:
-                # find matching [
-                count = 1
-                i
-                while count > 0:
-                    i -= 1
-                    if code[i] == "]":
-                        count += 1
-                    elif code[i] == "[":
-                        count -= 1
+            pass
+        i += 1
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        with open(sys.argv[1], 'r') as file:
+            print(bf(file.read()))
     else:
-        pass
-    i += 1
+        print("Usage: python3 bf_intp.py [file]")
