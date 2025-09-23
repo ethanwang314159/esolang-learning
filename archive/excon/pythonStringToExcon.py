c7fed7050840d60c871e6777b943d90c8594bf51
ethanwang314159@gmail.com <228432047+ethanwang314159@users.noreply.github.com>
2025-09-23 17:36:58 +1000

    I didn't like the other commit message lol

--- archive/excon/pythonStringToExcon.py
+++ archive/excon/pythonStringToExcon.py
@@ -1,1 +1,17 @@
 print("".join([":" + "".join(["^<" if b == "1" else "<" for b in bin(ord(l))[2:].zfill(8)[::-1]]) + '!' for l in input()]))
+"""
+make it make sense
+print(
+    "".join(
+        [":" + "".join(
+            ["^<" if b == "1" else 
+             "<" for b in 
+             bin(ord(l))[2:].zfill(8)[::-1]
+             ]
+            ) + 
+            '!' for l in input()
+            ]
+        )
+    )
+
+"""
