ef7f554959c29db11209a225b0bcdbf08f2d6c57
ethanwang314159@gmail.com <228432047+ethanwang314159@users.noreply.github.com>
2025-09-24 11:14:26 +1000

    rust intp

---
+++ (9)0 bytes XD/intp.rs
@@ -0,0 +1,20 @@
+use std::io; 
+
+fn main() {
+    
+    println!("Please enter some text:"); 
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input) // Read a line from stdin into user_input
+        .expect("Failed to read line"); 
+    let input = user_input.trim();
+    
+    if input == "0" {
+        println!("0");
+    }
+    if input == "1" {
+        while 7 == 7 {
+            println!("1");
+        }
+    }
+}
